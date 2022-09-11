15ee2e1ca6141c12bc02728e9c2dc7019e2b6e4b
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-11 14:39:23 -0700

    add splash screen

---
+++ screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+function SplashScreen() {
+    return (
+        <View>
+        </View>
+    )
+}
+export default SplashScreen;
