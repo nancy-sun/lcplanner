4fcd135d847197e85ab361ca11c8aadcc607cd22
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-11 14:49:18 -0700

    modified splash screen

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 function SplashScreen() {
+
+    const navigate = useNavigation();
+
+    const isLoggedIn = () => {
+        return false;
+    }
+
+    useEffect(() => {
+        if (isLoggedIn()) {
+            navigate.navigate("Tasks");
+        } else {
+            navigate.navigate("SignIn");
+        }
+    }, [])
+
     return (
-        <View>
+        <View style={styles.container}>
+            <ActivityIndicator />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 9,
+        justifyContent: "center",
+        backgroundColor: "white",
+        width: "100%"
+    }
+});
+
 export default SplashScreen;
