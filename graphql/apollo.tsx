2182f570ea8c26e1b2717a085849129c08feee86
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-13 21:41:08 -0700

    add auth to apollo http client

--- graphql/apollo.tsx
+++ graphql/apollo.tsx
@@ -1,10 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { GRAPHQL_URI } from '@env';
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { GRAPHQL_URI } from "@env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
     uri: GRAPHQL_URI,
-    cache: new InMemoryCache(),
 });
 
+const authLink = setContext(async (_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = await AsyncStorage.getItem("token");
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token || "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
 
 export { client };
