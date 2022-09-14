c430e50268acbafc5aa49ad9f91f29689d495253
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-09-13 23:02:22 -0700

    add Create new task mutation

--- graphql/mutations.tsx
+++ graphql/mutations.tsx
@@ -25,5 +25,21 @@ const SIGN_IN_MUTATION = gql`mutation SignIn($email: String!, $password: String!
     }
 }`;
 
-export { SIGN_UP_MUTATION, SIGN_IN_MUTATION };
+const CREATE_TASK_MUTATION = gql`
+mutation CreateTask($title: String!, $tasksListId: ID!, $date: String!, $deadline: String, $note: String) {
+    createTask(title: $title, tasksListID: $tasksListId, date: $date, deadline: $deadline, note: $note) {
+        id
+        date
+        title
+        deadline
+        note
+        isCompleted
+        tasksList {
+            id
+            progress
+        }
+    }
+}`
+
+export { SIGN_UP_MUTATION, SIGN_IN_MUTATION, CREATE_TASK_MUTATION };
 
