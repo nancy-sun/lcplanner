18eefed37ddd0fb5b590e3419447b83a87218043
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-10-10 22:06:22 -0700

    set up chart and edit example.env

--- components/ProfilePieChart/ProfilePieChart.tsx
+++ components/ProfilePieChart/ProfilePieChart.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Dimensions } from 'react-native';
+import { useQuery } from '@apollo/client';
 import {
     LineChart,
     BarChart,
@@ -9,16 +10,46 @@ import {
     StackedBarChart
 } from "react-native-chart-kit";
 
+
 function ProfilePieChart() {
 
+    const screenWidth = Dimensions.get("window").width;
 
+    const getLCProgress = () => {
+
+    }
+
+    useEffect(() => {
+        getLCProgress();
+    }, [])
 
     return (
         <View>
-            <Text>PieChart</Text>
+            {/* <PieChart
+                // data={data}
+                width={screenWidth}
+                height={220}
+                chartConfig={chartConfig}
+                accessor={"population"}
+                backgroundColor={"transparent"}
+                paddingLeft={"15"}
+                center={[10, 50]}
+                absolute
+            /> */}
         </View>
     )
 }
+
+const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+};
 
 
 export default ProfilePieChart;
