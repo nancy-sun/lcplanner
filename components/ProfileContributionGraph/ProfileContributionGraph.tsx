d3f8c669765f94d6fbcfe3d90193157e3d0e80c8
Nancy Sun <99620863+nancy-sun@users.noreply.github.com>
2022-10-21 01:12:10 -0700

    edit contribution graph

--- components/ProfileContributionGraph/ProfileContributionGraph.tsx
+++ components/ProfileContributionGraph/ProfileContributionGraph.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { View } from "react-native";
 import { Dimensions } from "react-native";
 import { ContributionGraph } from "react-native-chart-kit";
-// import { ContributionChartValue } from "react-native-chart-kit/dist/contribution-graph/ContributionGraph";
 
 function ProfileContributionGraph() {
     const screenWidth = Dimensions.get("window").width;
@@ -23,7 +22,7 @@ function ProfileContributionGraph() {
     ];
 
     return (
-        <View style={{ backgroundColor: "white" }}>
+        <View>
             <ContributionGraph
                 values={commitsData}
                 endDate={new Date("2017-04-01")}
@@ -32,19 +31,10 @@ function ProfileContributionGraph() {
                 height={220}
                 showOutOfRangeDays
                 style={{
-                    marginVertical: 8,
                     borderRadius: 16,
-                    backgroundColor: "white",
+                    marginLeft: -22
                 }}
-                chartConfig={{
-                    backgroundColor: "white",
-                    backgroundGradientFrom: "white",
-                    backgroundGradientTo: "white",
-                    backgroundGradientFromOpacity: 0,
-                    backgroundGradientToOpacity: 0,
-                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                }}
+                chartConfig={chartConfig}
             // accessor={}
             />
         </View>
@@ -52,15 +42,10 @@ function ProfileContributionGraph() {
 };
 
 const chartConfig = {
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-    backgroundGradientFrom: "transparent",
     backgroundGradientFromOpacity: 0,
-    backgroundGradientTo: "transparent",
     backgroundGradientToOpacity: 0,
-    propsForBackgroundLines: {
-        stroke: "rgba(140,140,140,.5)",
-        strokeWidth: 1
-    },
+    color: (opacity = 1) => `rgba(240, 154, 42, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
 };
 
 export default ProfileContributionGraph;
